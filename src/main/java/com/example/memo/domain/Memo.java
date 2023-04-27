18a32753fa4d3dbd6797951a2db9ec50d85bbd2b
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-04-27 23:03:58 +0900

    feat: 생성자 추가

--- src/main/java/com/example/memo/domain/Memo.java
+++ src/main/java/com/example/memo/domain/Memo.java
@@ -3,9 +3,18 @@ package com.example.memo.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter @Setter
+@Entity
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String title;
     private String content;
 
@@ -13,5 +22,14 @@ public class Memo {
         this.id = id;
         this.title = title;
         this.content = content;
+    }
+
+    public Memo(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Memo() {
+
     }
 }
