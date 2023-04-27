126dd44d1b066e84375b77680ce2a875c71ce8ac
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-04-27 23:02:08 +0900

    fix: JpaRepository로 상속받는 메소드는 정의해주지 않음

--- src/main/java/com/example/memo/repository/MemoRepository.java
+++ src/main/java/com/example/memo/repository/MemoRepository.java
@@ -1,9 +1,10 @@
 package com.example.memo.repository;
 
 import com.example.memo.domain.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemoRepository {
-
+public interface MemoRepository extends JpaRepository<Memo, String> {
+    Memo findByTitle(String title);
 }
