808931150150e94c733a5bd9e1b09d379bf3a703
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-04-27 23:03:06 +0900

    feat: MemoService에 create, findOneMemo 메소드 추가

--- src/main/java/com/example/memo/service/MemoService.java
+++ src/main/java/com/example/memo/service/MemoService.java
@@ -1,5 +1,6 @@
 package com.example.memo.service;
 
+import com.example.memo.domain.Memo;
 import com.example.memo.repository.MemoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,17 @@ public class MemoService {
     @Autowired
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
+    }
+
+    /**
+     * Create Memo
+     * @param memo
+     */
+    public Memo create(Memo memo) {
+        return memoRepository.save(memo);
+    }
+
+    public Memo findOneMemo(String title) {
+        return memoRepository.findByTitle(title);
     }
 }
