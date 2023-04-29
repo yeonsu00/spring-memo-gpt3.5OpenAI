7589b2fd85d0daba45b5fc1edac2157d431bced9
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-04-30 00:08:52 +0900

    feat: MemoServiceTest 메모 읽기 테스트 코드 작성

--- src/test/java/com/example/memo/service/MemoServiceTest.java
+++ src/test/java/com/example/memo/service/MemoServiceTest.java
@@ -5,8 +5,13 @@ import com.example.memo.repository.MemoRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -35,6 +40,20 @@ public class MemoServiceTest {
 
         // Assert that the createdMemo object is not null
         assertEquals(memo, createdMemo);
+    }
+
+    @Test
+    public void testFindAllMemo() {
+        List<Memo> memoList = new ArrayList<>();
+        memoList.add(new Memo("Test Memo 1", "Test content 1"));
+        memoList.add(new Memo("Test Memo 2", "Test content 2"));
+        memoList.add(new Memo("Test Memo 3", "Test content 3"));
+        Mockito.when(memoRepository.findAll()).thenReturn(memoList);
+        List<Memo> result = memoService.findAllMemo();
+        assertEquals(3, result.size());
+        assertEquals("Test Memo 1", result.get(0).getTitle());
+        assertEquals("Test content 2", result.get(1).getContent());
+        assertEquals("Test Memo 3", result.get(2).getTitle());
     }
 
 }
