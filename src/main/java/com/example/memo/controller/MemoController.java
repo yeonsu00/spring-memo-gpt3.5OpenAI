b7378e500ab5276256de0e3eccbbab83c7614c6e
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-06-04 18:56:52 +0900

    feat: 메모 생성 기능 수정 - date 추가

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -4,14 +4,15 @@ import com.example.memo.domain.Memo;
 import com.example.memo.service.MemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class MemoController {
 
     private final MemoService memoService;
@@ -25,7 +26,9 @@ public class MemoController {
     public String createMemo(@RequestBody Map<String, String> memo) {
         String title = memo.get("title");
         String content = memo.get("content");
-        Memo newMemo = new Memo(title, content);
+        Date date = new Date();
+        System.out.println(date);
+        Memo newMemo = new Memo(title, content, date);
         memoService.createMemo(newMemo);
         return "create";
     }
